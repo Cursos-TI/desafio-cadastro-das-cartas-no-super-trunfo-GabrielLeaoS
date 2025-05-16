605695b1e77c170189511ecb31f09893a08da52d
GabrielLeaoS <202504452151@alunos.newtonpaiva.edu.br>
2025-05-16 02:29:50 +0000

    Criei o Jogo Super Trunfo 1.0

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -7,6 +7,47 @@
 //Teste larissa
 
 int main() {
+    printf("Desafio Super Trunfo \n");
+
+    char Estado[50];
+    char Codigo_da_carta[50];
+    char Nome_da_cidade[50];
+    int Populacao;
+    float Area_em_km;
+    float PIB;
+    int Numero_de_pontos_turisticos; 
+
+    printf("Digite o nome do Estado: ");
+    scanf("%s", Estado);   
+    printf("O Estado e: %s\n", Estado); 
+
+    printf("Digite o codigo da Carta: ");
+    scanf("%s", Codigo_da_carta);
+    printf("O codigo da Carta e: %s\n", Codigo_da_carta);  
+
+    printf("Digitei o nome da Cidade: ");
+    scanf("%s", Nome_da_cidade);
+    printf("O nome da Cidade e: %s\n", Nome_da_cidade);  
+    
+    printf("Digite a Populacao: ");
+    scanf("%d", &Populacao);  
+    printf("A Populacao e: %d\n", Populacao); 
+
+    printf("Digite a Area em Km: ");
+    scanf("%f", &Area_em_km);  
+    printf("A Area em Km e: %f\n", Area_em_km); 
+
+    printf("Digite o PIB: ");
+    scanf("%f", &PIB);  
+    printf("O PIB e: %f\n", PIB); 
+
+    printf("Digite o numero de pontos Turisticos: ");
+    scanf("%d", &Numero_de_pontos_turisticos);
+    printf("O numero de Pontos Turisticos e: %d\n", Numero_de_pontos_turisticos);
+
+    printf("O nome da Estado e: %s - O codigo da carta e: %s - O nome da cidade e: %s\n", Estado, Codigo_da_carta, Nome_da_cidade);
+    printf("A Populacao e: %d - A Area em Km e: %f - O PIB e: %f\n", Populacao, Area_em_km, PIB);
+    printf("O numero de Pontos Turisticos e: %d\n", Numero_de_pontos_turisticos);
     // Sugestão: Defina variáveis separadas para cada atributo da cidade.
     // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
     
